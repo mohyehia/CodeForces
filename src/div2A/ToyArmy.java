30d91697f1dcad8b0510986dd7c80a9baad30cc9
mohyehia <mohammedyehia99@gmail.com>
2017-03-09 19:26:05 +0200

    Problem Link
    http://codeforces.com/contest/84/problem/A

---
+++ src/div2A/ToyArmy.java
@@ -0,0 +1,65 @@
+package div2A;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class ToyArmy {
+	public static void main(String[] args) {
+		out = new PrintWriter(new BufferedOutputStream(System.out));
+		Reader in = new Reader();
+		int n = in.readInt();
+		out.println(n + n / 2);
+		out.close();
+		in.close();
+	}
+	static PrintWriter out;
+	static class Reader {
+		BufferedReader br;
+		StringTokenizer st;
+		public Reader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+		String read() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+		int readInt() {
+			return Integer.parseInt(read());
+		}
+		long readLong() {
+			return Long.parseLong(read());
+		}
+		double readDouble() {
+			return Double.parseDouble(read());
+		}
+		char readChar() {
+			return read().charAt(0);
+		}
+		String readLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+		void close() {
+			try {
+				br.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
