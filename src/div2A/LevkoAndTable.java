d82ba2f3ef3b16db6f6d427fb04f17ff5c73f141
mohyehia <mohammedyehia99@gmail.com>
2017-03-09 20:29:50 +0200

    Problem Link
    http://codeforces.com/contest/361/problem/A

---
+++ src/div2A/LevkoAndTable.java
@@ -0,0 +1,71 @@
+package div2A;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class LevkoAndTable {
+	public static void main(String[] args) {
+		out = new PrintWriter(new BufferedOutputStream(System.out));
+		Reader in = new Reader();
+		int n = in.readInt(), k = in.readInt();
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				if(i == j) out.print(k + " ");
+				else out.print(0 + " ");
+			}
+			out.println();
+		}
+		out.close();
+		in.close();
+	}
+	static PrintWriter out;
+	static class Reader {
+		BufferedReader br;
+		StringTokenizer st;
+		public Reader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+		String read() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+		int readInt() {
+			return Integer.parseInt(read());
+		}
+		long readLong() {
+			return Long.parseLong(read());
+		}
+		double readDouble() {
+			return Double.parseDouble(read());
+		}
+		char readChar() {
+			return read().charAt(0);
+		}
+		String readLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+		void close() {
+			try {
+				br.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
