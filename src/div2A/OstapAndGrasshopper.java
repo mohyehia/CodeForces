1b6ee1f2a9d62e4c38f310dcccc31a85bde642c7
mohyehia <mohammedyehia99@gmail.com>
2017-03-20 14:20:53 +0200

    Problem Link
    http://codeforces.com/contest/735/problem/A

---
+++ src/div2A/OstapAndGrasshopper.java
@@ -0,0 +1,111 @@
+package div2A;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class OstapAndGrasshopper {
+	public static void main(String[] args) {
+		out = new PrintWriter(new BufferedOutputStream(System.out));
+		Reader in = new Reader();
+		int n = in.readInt(), k = in.readInt();
+		String s = in.read();
+		int min = Math.min(s.indexOf('T'), s.indexOf('G'));
+		int max = Math.max(s.indexOf('T'), s.indexOf('G'));
+		String ss = s.substring(min, max + 1);
+		if(k >= ss.length()) out.println("NO");
+		else{
+			if((max - min) % k == 0){
+				for(int i = 0; i <= max; i += k){
+					if(ss.charAt(i) == '#'){
+						System.out.println("NO");
+						System.exit(0);
+					}
+					if(i  + k >= ss.length()) break;
+				}
+				out.println("YES");
+			}else out.println("NO");
+		}
+		in.close();
+		out.close();
+	}
+	static PrintWriter out;
+	static class Reader {
+		BufferedReader br;
+		StringTokenizer st;
+		public Reader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+		String read() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+		int readInt() {
+			return Integer.parseInt(read());
+		}
+		long readLong() {
+			return Long.parseLong(read());
+		}
+		double readDouble() {
+			return Double.parseDouble(read());
+		}
+		char readChar() {
+			return read().charAt(0);
+		}
+		int[] readIntArray(int n) {
+			int[] a = new int[n];
+			for (int i = 0; i < n; i++)
+				a[i] = readInt();
+			return a;
+		}
+		long[] readLongArray(int n) {
+			long[] a = new long[n];
+			for (int i = 0; i < n; i++)
+				a[i] = readLong();
+			return a;
+		}
+		double[] readDoubleArray(int n) {
+			double[] a = new double[n];
+			for (int i = 0; i < n; i++)
+				a[i] = readDouble();
+			return a;
+		}
+		String[] readStringArray(int n) {
+			String[] s = new String[n];
+			for (int i = 0; i < n; i++)
+				s[i] = read();
+			return s;
+		}
+		char[] readCharArray(int n) {
+			char[] ch = new char[n];
+			for (int i = 0; i < n; i++)
+				ch[i] = readChar();
+			return ch;
+		}
+		String readLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+		void close() {
+			try {
+				br.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
