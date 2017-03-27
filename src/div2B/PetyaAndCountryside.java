5f1b6fdecb404d61aaf7e427f547ee2e06eb6a04
mohyehia <mohammedyehia99@gmail.com>
2017-03-28 01:25:11 +0200

    Problem Link
    http://codeforces.com/contest/66/problem/B

---
+++ src/div2B/PetyaAndCountryside.java
@@ -0,0 +1,124 @@
+package div2B;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class PetyaAndCountryside {
+	public static void main(String[] args) {
+		out = new PrintWriter(new BufferedOutputStream(System.out));
+		Reader in = new Reader();
+		int n = in.readInt();
+		int[] a = in.readIntArray(n);
+		if(n == 1){
+			System.out.println(1);
+			return;
+		}
+		int max = 0, count = 1;
+		for(int i = 1; i < n - 1; i++){
+			for(int j = i; j > 0; j--){
+				if(a[j] >= a[j - 1]) count++;
+				else break;
+			}
+			for(int k = i; k < n - 1; k++){
+				if(a[k] >= a[k + 1]) count++;
+				else break;
+			}
+			max = Math.max(max, count);
+			count = 1;
+		}
+		for(int i = 0; i < n - 1; i++){
+			if(a[i] >= a[i + 1]) count++;
+			else break;
+		}
+		max = Math.max(max, count);
+		count = 1;
+		for(int i = n - 1; i > 0; i--){
+			if(a[i] >= a[i - 1]) count++;
+			else break;
+		}
+		max = Math.max(max, count);
+		out.println(max);
+		in.close();
+		out.close();
+	}
+	static PrintWriter out;
+	static class Reader {
+		BufferedReader br;
+		StringTokenizer st;
+		public Reader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+		String read() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+		int readInt() {
+			return Integer.parseInt(read());
+		}
+		long readLong() {
+			return Long.parseLong(read());
+		}
+		double readDouble() {
+			return Double.parseDouble(read());
+		}
+		char readChar() {
+			return read().charAt(0);
+		}
+		int[] readIntArray(int n) {
+			int[] a = new int[n];
+			for (int i = 0; i < n; i++)
+				a[i] = readInt();
+			return a;
+		}
+		long[] readLongArray(int n) {
+			long[] a = new long[n];
+			for (int i = 0; i < n; i++)
+				a[i] = readLong();
+			return a;
+		}
+		double[] readDoubleArray(int n) {
+			double[] a = new double[n];
+			for (int i = 0; i < n; i++)
+				a[i] = readDouble();
+			return a;
+		}
+		String[] readStringArray(int n) {
+			String[] s = new String[n];
+			for (int i = 0; i < n; i++)
+				s[i] = read();
+			return s;
+		}
+		char[] readCharArray(int n) {
+			char[] ch = new char[n];
+			for (int i = 0; i < n; i++)
+				ch[i] = readChar();
+			return ch;
+		}
+		String readLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+		void close() {
+			try {
+				br.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
