60977bc0583ed7873d0e299ae25e774cba8d9511
mohyehia <mohammedyehia99@gmail.com>
2017-05-04 17:31:44 +0200

    Solve New Problems

---
+++ src/div2A/K_String.java
@@ -0,0 +1,106 @@
+package div2A;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class K_String {
+	public static void main(String[] args) {
+		out = new PrintWriter(new BufferedOutputStream(System.out));
+		Reader in = new Reader();
+		int k = in.readInt();
+		int[] counts = new int[26];
+		for(char ch : in.read().toCharArray()) counts[ch - 'a']++;
+		for(int i : counts) {
+			if(i % k != 0){
+				System.out.println(-1);
+				System.exit(0);
+			}
+		}
+		for(int i = 0; i < k; i++)
+			for(int j = 0; j < 26; j++)
+				for(int l = 0; l < counts[j] / k; l++) 
+					out.print((char) ('a' + j));
+		in.close();
+		out.close();
+	}
+	static PrintWriter out;
+	static class Reader {
+		BufferedReader br;
+		StringTokenizer st;
+		public Reader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+		String read() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+		int readInt() {
+			return Integer.parseInt(read());
+		}
+		long readLong() {
+			return Long.parseLong(read());
+		}
+		double readDouble() {
+			return Double.parseDouble(read());
+		}
+		char readChar() {
+			return read().charAt(0);
+		}
+		int[] readIntArray(int n) {
+			int[] a = new int[n];
+			for (int i = 0; i < n; i++)
+				a[i] = readInt();
+			return a;
+		}
+		long[] readLongArray(int n) {
+			long[] a = new long[n];
+			for (int i = 0; i < n; i++)
+				a[i] = readLong();
+			return a;
+		}
+		double[] readDoubleArray(int n) {
+			double[] a = new double[n];
+			for (int i = 0; i < n; i++)
+				a[i] = readDouble();
+			return a;
+		}
+		String[] readStringArray(int n) {
+			String[] s = new String[n];
+			for (int i = 0; i < n; i++)
+				s[i] = read();
+			return s;
+		}
+		char[] readCharArray(int n) {
+			char[] ch = new char[n];
+			for (int i = 0; i < n; i++)
+				ch[i] = readChar();
+			return ch;
+		}
+		String readLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+		void close() {
+			try {
+				br.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
