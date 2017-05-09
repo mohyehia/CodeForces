f63b1110de09e1007e5d560309f801c876bf94a9
mohyehia <mohammedyehia99@gmail.com>
2017-05-09 06:00:57 +0200

    Solve New B Problem

---
+++ src/div2B/InnaAndNewMatrixOfCandies.java
@@ -0,0 +1,106 @@
+package div2B;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class InnaAndNewMatrixOfCandies {
+	public static void main(String[] args) {
+		out = new PrintWriter(new BufferedOutputStream(System.out));
+		Reader in = new Reader();
+		int n = in.readInt(), m = in.readInt();
+		HashSet<Integer> set = new HashSet<>();
+		while(n > 0){
+			String s = in.read();
+			int diff = s.indexOf('S') - s.indexOf('G');
+			if(diff < 0) {
+				System.out.println(-1); return;
+			}
+			set.add(diff);
+			n--;
+		}
+		out.println(set.size());
+		in.close();
+		out.close();
+	}
+	static PrintWriter out;
+	static class Reader {
+		BufferedReader br;
+		StringTokenizer st;
+		public Reader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+		String read() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+		int readInt() {
+			return Integer.parseInt(read());
+		}
+		long readLong() {
+			return Long.parseLong(read());
+		}
+		double readDouble() {
+			return Double.parseDouble(read());
+		}
+		char readChar() {
+			return read().charAt(0);
+		}
+		int[] readIntArray(int n) {
+			int[] a = new int[n];
+			for (int i = 0; i < n; i++)
+				a[i] = readInt();
+			return a;
+		}
+		long[] readLongArray(int n) {
+			long[] a = new long[n];
+			for (int i = 0; i < n; i++)
+				a[i] = readLong();
+			return a;
+		}
+		double[] readDoubleArray(int n) {
+			double[] a = new double[n];
+			for (int i = 0; i < n; i++)
+				a[i] = readDouble();
+			return a;
+		}
+		String[] readStringArray(int n) {
+			String[] s = new String[n];
+			for (int i = 0; i < n; i++)
+				s[i] = read();
+			return s;
+		}
+		char[] readCharArray(int n) {
+			char[] ch = new char[n];
+			for (int i = 0; i < n; i++)
+				ch[i] = readChar();
+			return ch;
+		}
+		String readLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+		void close() {
+			try {
+				br.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
